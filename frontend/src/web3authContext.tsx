f2d02f389ed6dd93a7dc1fa7c81fa8a1bdc3e034
vivi <pollivivian@gmail.com>
2025-02-20 15:08:27 -0300

    fix modal auth and add readme

--- frontend/src/web3authContext.tsx
+++ frontend/src/web3authContext.tsx
@@ -1,40 +1,36 @@
 import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } from "@web3auth/base";
-import { getDefaultExternalAdapters } from "@web3auth/default-evm-adapter";
-import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 import { Web3AuthOptions } from "@web3auth/modal";
+import { SolanaPrivateKeyProvider } from "@web3auth/solana-provider";
 
 const clientId =
   "BNZ3ahrG36eqnHudabgklQPBTudgIlW_kj_-N-R98cUwA0IWqcpBs5zk1-IulVt5dFhCMfKopcDLvuZm8okP4J0";
 
 const chainConfig = {
-  chainNamespace: CHAIN_NAMESPACES.EIP155,
-  chainId: "0xaa36a7",
-  rpcTarget: "https://rpc.ankr.com/eth_sepolia",
-  displayName: "Ethereum Sepolia Testnet",
-  blockExplorerUrl: "https://sepolia.etherscan.io",
-  ticker: "ETH",
-  tickerName: "Ethereum",
-  logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+  chainNamespace: CHAIN_NAMESPACES.SOLANA,
+  chainId: "devnet",
+  rpcTarget: "https://api.devnet.solana.com",
+  displayName: "Solana Devnet",
+  blockExplorerUrl: "https://explorer.solana.com/?cluster=devnet",
+  ticker: "SOL",
+  tickerName: "Solana",
+  logo: "https://cryptologos.cc/logos/solana-sol-logo.png",
 };
 
-const privateKeyProvider = new EthereumPrivateKeyProvider({
-  config: {
-    chainConfig,
-  },
+const privateKeyProvider = new SolanaPrivateKeyProvider({
+  config: { chainConfig },
 });
 
 const web3AuthOptions: Web3AuthOptions = {
   chainConfig,
   clientId,
-  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+  enableLogging: true,
   privateKeyProvider,
 };
 
-const adapters = getDefaultExternalAdapters({ options: web3AuthOptions });
-
 const web3AuthContextConfig = {
   web3AuthOptions,
-  adapters: [...adapters],
+  adapters: [],
 };
 
 export default web3AuthContextConfig;
